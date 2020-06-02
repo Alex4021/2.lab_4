b42e411709c2fe54079ee93f38158b342f551791
Alex4021 <Alex4021@users.noreply.github.com>
2020-06-02 15:19:02 +0300

    Добавьте файлы проекта.

---
+++ lab_4.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stdarg.h>
+using namespace std;
+void ab1(int a,int b)
+{
+	if (a > b)
+	{
+		a = a * 5;
+		b = b - 5;
+	}
+	else
+	{
+		a = a - 5;
+		b = b * 5;
+	}
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+}
+
+void ab(int& a, int& b)
+{
+	if (a > b)
+	{
+		a = a * 5;
+		b = b - 5;
+	}
+	else
+	{
+		a = a - 5;
+		b = b * 5;
+	}
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+}
+
+int main()
+{
+	int a, b;
+	cout << "Enter a,b" << endl;
+	cout << "a= ";
+	cin >> a;
+	cout << "b= ";
+	cin >> b;
+	ab(a, b);
+	ab1(a, b);
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+}
